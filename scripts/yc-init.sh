66538cc30e40093900b2c0c292a0ad656b81565c
Vladislav Nagorny <u.slash.vlad@gmail.com>
2024-05-19 20:28:11 +0300

    Fixed lockbox order in init scripts

--- scripts/yc-init.sh
+++ scripts/yc-init.sh
@@ -2,7 +2,7 @@
 
 cd $YC_VM_WORKDIR
 
+yc lockbox payload get --id $YC_SECRETS_ID --key GARNET_USERAPI_ENV >.env
+
 docker compose pull
 docker compose up -d
-
-yc lockbox payload get --id $YC_SECRETS_ID --key GARNET_USERAPI_ENV > .env
